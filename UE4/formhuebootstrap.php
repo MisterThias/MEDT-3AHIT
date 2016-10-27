0cef688c0fb166e775bcc0ad9da3bb902e9029bb
MisterThias <matthias.kretschmayer@gmx.at>
2016-10-27 15:01:37 +0200

    First File

---
+++ UE4/formhuebootstrap.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML5>
+
+<html>
+	<header>
+	<meta charset="utf-8">
+	<title>PHP Form Demo 1</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+	</header>
+	<body>
+	<div class="container-fluid">
+		<div class="row">
+		<form class="form-inline" action="http://127.0.0.1/MEDT/UE4/formhuebootstrap.php">
+  			<div class="form-group col-lg-12 col-xs-12">
+    			<input type="text" class="form-control" id="InputFirstName" placeholder="Your first name" style="width:100%;">
+  			</div>
+  			<div class="form-group col-lg-12 col-xs-12">
+       			<input type="text" class="form-control" id="InputLastName" placeholder="Your last name" style="width:100%;">
+  			</div>
+  			<div class="text-center">
+  			<button type="submit" class="btn btn-default">Log in</button>
+			</div>
+		</form>
+		</div>	
+	</div>
+	</body>
+</html>
