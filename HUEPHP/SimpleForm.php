8d1eec9105cebfbadd507897996fa71ead6380b6
MisterThias <matthias.kretschmayer@gmx.at>
2016-11-16 20:59:50 +0100

    First File

--- HUEPHP/SimpleForm.php
+++ HUEPHP/SimpleForm.php
@@ -81,14 +81,14 @@
 			</div>
 			</div>
 		</form>
-
+		<div class="row">
 		<?php 
 
 		if(isset($_GET['submitter'])){
-			echo "<div class=\"jumbotron animated fadeInUp\"><h1 style=\"color:#b30000;\">".$_GET['vn']."<small> ".$_GET['nn']."</small></h1><p>Your email address is: <span style=\"color:#b30000;\">".$_GET['em']."</span>";}
+			echo "<div class=\"container animated fadeInUp col-xs-12\"><h1 style=\"color:#b30000;\">".$_GET['vn']."<small> ".$_GET['nn']."</small></h1><p>Your email address is: <span style=\"color:#b30000;\">".$_GET['em']."</span>";}
 			
 
-		?>
+		?></div>
 		</div>
 		</div>
 		</div>	
---
+++ HUEPHP/SysEnvInfo.php
@@ -0,0 +1,68 @@
+<!DOCTYPE HTML5>
+
+<html>
+	<header>
+	<meta charset="utf-8">
+	<title>PHP Form Demo 1</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+	</header>
+	
+	<body>
+	<div class="container-fluid">
+		
+		<div class="container">
+		<div class="page-header">
+			<h1 style="color:#b30000;">$_SERVER<small> im Überblick</small></h1>
+		</div> 
+		<div class="container-fluid">
+		<div class="row">
+		<div class="table-responsive">
+			<table class="table table-bordered table-hover">
+			<thead>
+				<tr class= "danger">
+				  <td class="col-xs-2"><p class="text-center ">Variable</p></td>
+				  <td class=" col-xs-10"><p class="text-center">Wert</p></td>
+				</tr>
+			</thead>
+			<tbody class = "table">
+				<tr>
+				  <td><p class="text-center">Skript-Pfad</p></td>
+				  <td><p class="text-center"><?php echo $_SERVER['SCRIPT_FILENAME']; ?></p></td>
+				</tr>
+				<tr>
+				  <td><p class="text-center">Server-Name/IP</p></td>
+				  <td><p class="text-center"><?php echo $_SERVER['SERVER_NAME']."/".$_SERVER['SERVER_ADDR']; ?></p></td>
+				</tr>
+				<tr>
+				  <td><p class="text-center">Protokoll</p></td>
+				  <td><p class="text-center"><?php echo $_SERVER['SERVER_PROTOCOL']; ?></p></td>
+				</tr>
+				<tr>
+				  <td><p class="text-center">Client-IP</p></td>
+				  <td><p class="text-center"><?php echo $_SERVER['REMOTE_ADDR']; ?></p></td>
+				</tr>
+				<tr>
+				  <td><p class="text-center">URL</p></td>
+				  <td><p class="text-center"><?php echo $_SERVER['PHP_SELF']; ?></p></td>
+				</tr>
+				<tr>
+				  <td><p class="text-center">Server-Info</p></td>
+				  <td><p class="text-center"><?php echo $_SERVER['SERVER_SIGNATURE']; ?></p></td>
+				</tr>
+			</tbody>
+				
+			</table>
+
+		</div>
+		</div>	
+		
+		</div>
+		</div>
+		</div>	
+	</div>
+	</body>
+</html>
